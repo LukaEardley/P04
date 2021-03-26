c38a93023ed8bf48120f1ae18947d2cb7dfba406
Eardley, Arlo A <arlo.eardley@entelect.co.za>
2021-03-26 13:44:26 +0200

    Created panes for making the UI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,18 +4,74 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-    public static void main(String[] args) {
-        launch(args);
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Hello World");
+    primaryStage.setScene(new Scene(root, 800, 800));
+    primaryStage.show();
+
+    Accordion accordion = new Accordion();
+
+    TitledPane pane1 = new TitledPane("Trip Information", this.createGridPane());
+    TitledPane pane2 = new TitledPane("Item Information", new Label("Show all cars available"));
+    accordion.getPanes().add(pane1);
+    accordion.getPanes().add(pane2);
+
+    VBox vBox = new VBox(accordion);
+    Scene scene = new Scene(vBox);
+
+    primaryStage.setScene(scene);
+
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+
+  public Label createNewLabel(String labelName) {
+    Label label1 = new Label(labelName);
+    return label1;
+  }
+
+  public TextField createNewTextField() {
+    TextField textField = new TextField();
+    textField.setDisable(true);
+    return textField;
+  }
+
+  public GridPane createGridPane() {
+
+    GridPane grd = new GridPane();
+    grd.getColumnConstraints().add(new ColumnConstraints(300)); // column 0 is 100 wide
+    grd.getColumnConstraints().add(new ColumnConstraints(300));
+
+    grd.add(this.createNewLabel("ID:"), 0, 0);
+    grd.add(this.createNewTextField(), 1, 0);
+
+    grd.add(this.createNewLabel("Courier Name:"), 0, 1);
+    grd.add(this.createNewTextField(), 1, 1);
+
+
+    grd.add(this.createNewLabel("Priority:"), 0, 2);
+    grd.add(this.createNewTextField(), 1, 2);
+    grd.setPrefHeight(100);
+
+    return grd;
+  }
 }
