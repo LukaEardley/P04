a9efebf42147bfd737a01cf42d27d0cc2231eade
Eardley, Arlo A <arlo.eardley@entelect.co.za>
2021-03-26 13:45:45 +0200

    Added in screen title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("Hello World");
+    primaryStage.setTitle("UWGA Trip Monitoring System");
     primaryStage.setScene(new Scene(root, 800, 800));
     primaryStage.show();
 
